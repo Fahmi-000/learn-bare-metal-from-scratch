582c21af8da1b3c458ad6522dcb68998ba2de07c
Fahmi F. <ffxxii10@gmail.com>
2024-10-27 22:19:04 +0700

    Menambahkan program blinking STM32

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#define RCCLEDPORT (RCC_GPIOC)
+#define LEDPORT (GPIOC)
+#define LEDPIN (GPIO13)
+#define GPIO_MODE_OUTPUT GPIO_MODE_OUTPUT_2_MHZ
+#define GPIO_PUPD_NONE GPIO_CNF_OUTPUT_PUSHPULL
+
+//static void rcc_setup(void) {
+    //rcc_clock_setup_pll (&rcc_hsi_configs [RCC_CLOCK_HSI_64MHZ]);
+
+//}
+
+static void gpio_setup (void){
+    rcc_periph_clock_enable(RCCLEDPORT);
+    gpio_set_mode(LEDPORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDPIN);
+}
+
+static void delay_cycle(uint32_t cycles){
+    //int i;
+    for (uint32_t i = 0; i < cycles; i++){
+        __asm__("nop");
+    }
+}
+
+int main (void) {
+    gpio_setup();
+    while (1)
+    {
+        gpio_toggle(LEDPORT, LEDPIN);
+        delay_cycle(1000000);
+    }
+    
+    return 0;
+}
